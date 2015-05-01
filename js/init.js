585b7a3509223eaaae69b11b7fdf307fe804c61e
Steven Dang <stevencdang@gmail.com>
2015-05-01 10:21:14 +0000

    completed v1 content and styling of main page

--- js/init.js
+++ js/init.js
@@ -14,5 +14,36 @@ $(document).ready(function() {
   $(".navbar .active a").css("color", "#000");
   $(".nav-humadata").css("padding", "5px 30px");
   var width = $(".nav-logo").width()
+  var width = $("#diagram").width();
+  var h = $("#diagram").height();
+  console.log(width);
+  console.log(h);
+  $("#proj-diagram").height(1.2*width);
   //$(".nav-logo").css("margin-left", "-" + width/2 + "px")
+  //
+  //Get max height
+  var height = 0;
+  var w = $(".hex-section").width();
+  $("#research-overview .hex-section").each(function() {
+    var h = $(this).height();
+    var w = $(this).width();
+    if (h > height) {
+      height = h;
+    }
+  });
+  $("#research-overview .hex-section").each(function() {
+    $(this).height(1.5*height);
+  });
+  height = 0;
+  var w = $(".hex-section").width();
+  $("#characterize .hex-section").each(function() {
+    var h = $(this).height();
+    var w = $(this).width();
+    if (h > height) {
+      height = h;
+    }
+  });
+  $("#characterize .hex-section").each(function() {
+    $(this).height(1.5*height);
+  });
 });
