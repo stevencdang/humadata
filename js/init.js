383f75e8f005e00c33196529b5bf4ca446fb3ab4
Steven Dang <stevencdang@gmail.com>
2015-05-05 00:54:38 +0000

    dynamic resizing of landing view

--- js/init.js
+++ js/init.js
@@ -1,8 +1,41 @@
-$(document).ready(function() {
+// By: Steven C. Dang
+// Copyright 2015
+//
+
+function resize_landing() {
+  var navHeight = $(".navbar").height();
   // Set the height of the landing page top section
   var width = $("#landing-view").width();
-  var height = width * 6/16;
-  $("#landing-view").height(height + "px");
+  var height = width * 6/16 + navHeight;
+  var caption_height = $("#landing-view .img-caption").outerHeight() 
+  var min = caption_height * 1.4 + navHeight;
+  if (height < min) {
+    console.log("setting to min height");
+    $("#landing-view").height(min);
+    console.log(caption_height)
+    $("#landing-view .img-caption").css(
+        "top", "0%"
+    );
+    var margin = ((min - caption_height - navHeight) / 2) + navHeight;
+    $("#landing-view .img-caption").css(
+        "margin-top", margin + "px"
+    );
+  } else {
+    console.log("setting to other height");
+    $("#landing-view").height(height);
+    $("#landing-view .img-caption").css(
+        "top", "100%"
+    );
+    $("#landing-view .img-caption").css(
+        "margin-top", "-" + 1.2 * caption_height + "px"
+    );
+  }
+
+};
+
+function resize() {
+  console.log("Resizing window elements");
+  resize_landing();
 
   $("#proj-diagram.blur").height(
       $("#proj-diagram.blur").parent().height())
@@ -46,4 +79,11 @@ $(document).ready(function() {
   $("#characterize .hex-section").each(function() {
     $(this).height(1.5*height);
   });
+
+};
+
+$(document).ready(function() {
+  resize();
+  window.onresize = resize;
 });
+
